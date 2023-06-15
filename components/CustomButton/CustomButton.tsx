d09deba8cbaa8d5447da251e765a08b16515d87c
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-15 14:50:42 +0200

    fix hero layout, add navbar and footer to main layout

--- components/CustomButton/CustomButton.tsx
+++ components/CustomButton/CustomButton.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image';
 import { CustomButtonProps } from '@/types';
 
 
-const CustomButton:React.FC<CustomButtonProps> = ({title, containerStyles, handleClick}) => {
+const CustomButton:React.FC<CustomButtonProps> = ({title, containerStyles, handleClick, btnType}) => {
   
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
