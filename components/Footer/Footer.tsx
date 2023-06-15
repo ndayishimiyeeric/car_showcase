7d04673884d492d6b2a2db11ddfaa0bc18a8bba7
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-15 15:23:39 +0200

    add footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { footerLinks } from '@/constants';
 
 type FooterProps = {
   
@@ -6,6 +9,43 @@ type FooterProps = {
 
 const Footer:React.FC<FooterProps> = () => {
   
-  return <div>Have a good coding</div>
+  return (
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-r-gray-100'>
+      <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+        <div className='flex flex-col justify-start items-start gap-6'>
+          <Image
+            src='/logo.svg'
+            alt='logo'
+            width={118}
+            height={18}
+            className='object-contain'
+          />
+          <p className='text-base text-gray-700'>
+            © 2023 CarHub. <br />All rights reserved.
+          </p>
+        </div>
+        <div className='footer__links'>
+          {footerLinks.map((link, index) => (
+            <div
+              key={index}
+              className='footer__link'
+            >
+              <h3 className='font-bold'>{link.title}</h3>
+              {link.links.map((item, index) => (
+                <Link key={index} href={item.url} className='text-gray-500'>{ item.title }</Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+        <p>@2023  CarHub. AllRights Reserved</p>
+        <div className='footer__copyrights-link'>
+          <Link href="/" className='text-gray-500'>Privacy Policy</Link>
+          <Link href="/" className='text-gray-500'>Terms of Use</Link>
+        </div>
+      </div>
+    </footer>
+  )
 }
 export default Footer;
