58df4f374b14bdfc4386b6b7623b2b3b40bc5ec8
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-15 14:51:07 +0200

    create the navbar

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type FooterProps = {
+  
+};
+
+const Footer:React.FC<FooterProps> = () => {
+  
+  return <div>Have a good coding</div>
+}
+export default Footer;
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 import Hero from "./Hero/Hero";
 import CustomButton from "./CustomButton/CustomButton";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
-export { Hero, CustomButton };
+export { Hero, CustomButton, Navbar, Footer };
