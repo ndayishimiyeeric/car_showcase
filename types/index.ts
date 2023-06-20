907df7f28ca2b5255086bcf022fd2ef2d566df3e
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-20 23:42:29 +0200

    add combobox and searchprops types

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+
+export interface SearchManufucturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
