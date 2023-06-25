8a3a3cb3ee7fb1dda30f0bdcfcd325e111a96d88
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-25 17:16:36 +0200

    add created components

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,14 @@ import Hero from "./Hero/Hero";
 import CustomButton from "./CustomButton/CustomButton";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
+import CustomFilter from "./CustomFilter/CustomFilter";
+import SearchBar from "./SearchBar/SearchBar";
+import SearchManufucturer from "./SearchManufucturer/SearchManufucturer";
+import CarCard from "./CarCard/CarCard";
+import CarDetails from "./CarDetails/CarDetails";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero, CustomButton, Navbar, Footer, CustomFilter,
+  SearchBar, SearchManufucturer, CarCard,
+  CarDetails
+};
