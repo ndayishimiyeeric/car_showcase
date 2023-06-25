86224f053eb23887cae602ef070432a4a0fa66b6
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-25 17:12:08 +0200

    Add a custom filter

---
+++ components/CustomFilter/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type CustomFilterProps = {
+  
+};
+
+const CustomFilter:React.FC<CustomFilterProps> = () => {
+  
+  return <div>Have a good coding</div>
+}
+export default CustomFilter;
