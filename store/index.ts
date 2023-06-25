0aa0ea76c71f227d4cda015556216925fad52490
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-25 17:37:23 +0200

    add get car image

--- store/index.ts
+++ store/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 
 const headers = {
@@ -24,4 +26,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageURL = (car: CarProps, angle?: string) => { 
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, model, year } = car;
+  
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return url.toString();
 };
