486a64bc21185c0eb07a791417da141274d91695
Ndayishimiye Eric <ndayishimiyeeric86@gmail.com>
2023-06-25 17:13:45 +0200

    add car types

--- types/index.ts
+++ types/index.ts
@@ -3,11 +3,35 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
+  textStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  icon?: string;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufucturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: string;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps;
 }
